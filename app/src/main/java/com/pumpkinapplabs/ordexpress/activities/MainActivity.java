ef4f0ef8766cd605d0d55eadc82884eb9c655255
renealberto10 <renealberto10@gmail.com>
2020-09-23 08:52:17 -0600

    Create menu main

--- app/src/main/java/com/pumpkinapplabs/ordexpress/activities/MainActivity.java
+++ app/src/main/java/com/pumpkinapplabs/ordexpress/activities/MainActivity.java
@@ -2,15 +2,25 @@ package com.pumpkinapplabs.ordexpress.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import com.pumpkinapplabs.ordexpress.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editext = findViewById(R.id.editext);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
+
+
 }
